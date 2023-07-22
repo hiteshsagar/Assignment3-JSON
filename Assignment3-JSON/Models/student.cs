5bccaf3e2f7db95d662c62cf02eb455ad53ab878
TheDevilCoder <200528076@student.georgianc.on.ca>
2023-07-21 23:55:39 -0400

    Add project files.

---
+++ Assignment3-JSON/Models/student.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment3_json.model
+{
+    internal class Student
+    {
+        public string Stdid = "200508307";
+        public string Fname = "Hitesh Sagar";
+        public string Lname = "Regulla";
+        public string Course = "BDAT";
+    }
+}
